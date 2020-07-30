c89afe27bc74ef0863529c802633207b8826aea5
Andrew Gargan <gargana@amazon.com>
2020-07-30 11:47:53 -0700

    Now we can deploy multiple instances to the same cluster with unique Helm release names

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -25,7 +25,7 @@ mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/
 kubectl version --short --client 
 
 while true; do
-  IP_ADDRESS=$(kubectl get pods vault-0 -o jsonpath={.status.podIP})
+  IP_ADDRESS=$(kubectl get pods vault-${RELEASE_NAME}-0 -o jsonpath={.status.podIP})
   if [ "${IP_ADDRESS}X" == "X" ]
   then
     sleep 1
@@ -40,13 +40,13 @@ until curl -fs -o /dev/null ${IP_ADDRESS}:8200/v1/sys/init; do
 done
 
 # See if vault is initialized
-init=$(kubectl exec -t vault-0 -- vault operator init -status)
+init=$(kubectl exec -t vault-${RELEASE_NAME}-0 -- vault operator init -status)
 
 echo "Is vault initialized: '${init}'"
 
 if [ "$init" != "Vault is initialized" ]; then
     echo "Initializing Vault"
-    SECRET_VALUE=$(kubectl exec vault-0 -- vault operator init -recovery-shares=${VAULT_NUMBER_OF_KEYS} -recovery-threshold=${VAULT_NUMBER_OF_KEYS_FOR_UNSEAL})
+    SECRET_VALUE=$(kubectl exec vault-${RELEASE_NAME}-0 -- vault operator init -recovery-shares=${VAULT_NUMBER_OF_KEYS} -recovery-threshold=${VAULT_NUMBER_OF_KEYS_FOR_UNSEAL})
     echo "storing vault init values in secrets manager"
     aws secretsmanager put-secret-value --region ${AWS_REGION} --secret-id ${VAULT_SECRET} --secret-string "${SECRET_VALUE}"
 else
@@ -68,7 +68,7 @@ if [ "$sealed" == "true" ]; then
     # Handle variable number of unseal keys
     for UNSEAL_KEY_INDEX in {1..${VAULT_NUMBER_OF_KEYS_FOR_UNSEAL}}
     do
-        kubectl exec vault-0 -- vault operator unseal $unseal_key[${UNSEAL_KEY_INDEX}] 
+        kubectl exec vault-${RELEASE_NAME}-0 -- vault operator unseal $unseal_key[${UNSEAL_KEY_INDEX}]
     done
 else
     echo "Vault is already unsealed"
@@ -78,13 +78,13 @@ VAULT_SECRET_VALUE=$(get_secret ${VAULT_SECRET})
 root_token=$(echo ${VAULT_SECRET_VALUE} | awk '{ if (match($0,/Initial Root Token: (.*)/,m)) print m[1] }' | cut -d " " -f 1)
 
 # Show who we have joined
-kubectl exec vault-0 -- vault login token=$root_token 2>&1 > /dev/null  # Hide this output from the console
+kubectl exec vault-${RELEASE_NAME}-0 -- vault login token=$root_token 2>&1 > /dev/null  # Hide this output from the console
 
 # Join other pods to the raft cluster
-kubectl exec -t vault-1 -- vault operator raft join http://vault-0.vault-internal:8200
+kubectl exec -t vault-${RELEASE_NAME}-1 -- vault operator raft join http://vault-${RELEASE_NAME}-0.vault-${RELEASE_NAME}-internal:8200
 
-kubectl exec -t vault-2 -- vault operator raft join http://vault-0.vault-internal:8200
+kubectl exec -t vault-${RELEASE_NAME}-2 -- vault operator raft join http://vault-${RELEASE_NAME}-0.vault-${RELEASE_NAME}-internal:8200
 
 # Show who we have joined
-kubectl exec -t vault-0 -- vault operator raft list-peers
+kubectl exec -t vault-${RELEASE_NAME}-0 -- vault operator raft list-peers
 # If we see 3 raft peers we have succeeded.
